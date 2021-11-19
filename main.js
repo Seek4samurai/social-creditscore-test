7f54c2121b3cf25c700f471944289b2b5cca6565
Seek4samurai <grvrawat999@gmail.com>
2021-11-19 16:19:44 +0530

    replay and outro done

--- main.js
+++ main.js
@@ -37,6 +37,7 @@ const quizData = [
     }
 ]
 
+
 let currentQuestion = 0;
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
@@ -44,12 +45,15 @@ const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
+const answerEls = document.querySelectorAll(".answer");
+const quiz = document.getElementById("quiz");
 
 let currentQuiz = 0;
 let score = 0;
 loadQuiz();
 
 function loadQuiz() {
+    deselectAnswers();
     const currentQuizData = quizData[currentQuiz];
     questionEl.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
@@ -59,8 +63,9 @@ function loadQuiz() {
 }
 
 function getSelected() {
-    const answerEls = document.querySelectorAll(".answer");
     let answer = undefined;
+
+
     answerEls.forEach(answerEl => {
         if (answerEl.checked) {
             answer = answerEl.id;
@@ -69,14 +74,29 @@ function getSelected() {
     return answer;
 }
 
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+};
+
 submitBtn.addEventListener("click", () => {
     const answer = getSelected();
+    if (answer === quizData[currentQuiz].correct) {
+        score++;
+    }
     if (answer) {
         currentQuiz++;
         if (currentQuiz < quizData.length) {
             loadQuiz();
         } else {
-            alert("You're done");
+            quiz.innerHTML = `<h2>Congratulation!<br>You Passed the test!<br>Your Social credit score is ${score}000</h2>
+            <style>.outro-buttons{display: inline; display: flex; flex-direction: row;}</style>
+            <div class = "outro-buttons">
+            <button onclick="location.reload()">Replay</button>
+            <button onclick="">See something even better</button>
+            </div>
+            `
         }
     }
 });
